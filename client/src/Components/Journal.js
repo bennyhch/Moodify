6b6b150e03f4a1800c7d107bab512171cacb4636
Moira <moira.fischer@hotmail.com>
2022-03-13 22:43:29 +0000

    refactored repeating functions

--- client/src/Components/Journal.js
+++ client/src/Components/Journal.js
@@ -4,7 +4,6 @@ import 'antd/dist/antd.css';
 import { Collapse } from 'antd';
 import WordCloud from 'react-d3-cloud'; 
 ///
-import moment from 'moment';
 import Entryitembehavior from './Entryitembehavior';
 import Entryitemevent from './Entryitemevent';
 import Entryitemthought from './Entryitemthought';
@@ -17,197 +16,37 @@ const { Panel } = Collapse;
 
 export default function Journal({entry}) {
 
-  const filteringSad = entry.filter(oneEntry => oneEntry.emotion === 'sad');
-  const filteringHappy = entry.filter(oneEntry => oneEntry.emotion === 'happy');
-  const filteringAnxious = entry.filter(oneEntry => oneEntry.emotion === 'anxious');
-
-  const dayOfTheWeekSad =  filteringSad.map((one) => moment(one.date).format('dddd'));
-  const countsSad = {};
-  dayOfTheWeekSad.forEach((x) => {
-    countsSad[x] = (countsSad[x] || 0) + 1;
-  });
-
-  const dayOfTheWeekHappy =  filteringHappy.map((one) => moment(one.date).format('dddd'));
-  const countsHappy = {};
-  dayOfTheWeekHappy.forEach((x) => {
-    countsHappy[x] = (countsHappy[x] || 0) + 1;
-  });
-
-  const dayOfTheWeekAnxious = filteringAnxious.map((one) => moment(one.date).format('dddd'));
-  const countsAnxious = {};
-  dayOfTheWeekAnxious.forEach((x) => {
-    countsAnxious[x] = (countsAnxious[x] || 0) + 1;
-  });
-//CHOAS STARTS FROM HERE ////////////////////////////////////////////////////
+	const filterEmotion = (emotion) => entry.filter(oneEntry => oneEntry.emotion === emotion);
 
 //Word Cloud
-	function splitingSentences (string) { 
-		let word = string.trim().split(" ");
-		return word;
-	}
-// Sad // Sad// Sad// Sad// Sad// Sad// Sad// Sad// Sad// Sad// Sad// Sad// Sad// Sad
-//Sad Event
-	const arrSadEvent = [];
-	filteringSad.forEach(oneEvent => {
-		arrSadEvent.push(splitingSentences(oneEvent.triggeringEvent))
-		return arrSadEvent;
-	});
-	const flattenedSadEvent = [].concat(...arrSadEvent);
-  const countsCloudSadEvent = {};
-	flattenedSadEvent.forEach((wrd) => {
-		countsCloudSadEvent[wrd] = (countsCloudSadEvent[wrd] || 0) + 1;
-	});
-	const arrCloudSadEvent = [];
-	for (const [key, value] of Object.entries(countsCloudSadEvent)) {
-		let objSadEvent = {text: `${key}`, value: `${value}`}
-		objSadEvent.value = (Number(objSadEvent.value)*1000);
-		arrCloudSadEvent.push(objSadEvent);
-	}
 
-// Sad Thought
-	const arrSadThought = [];
-	filteringSad.forEach(oneEvent => {
-		arrSadThought.push(splitingSentences(oneEvent.thought))
-		return arrSadThought;
-	});
-	const flattenedSadThought = [].concat(...arrSadThought);
-  const countsCloudSadThought = {};
-	flattenedSadThought.forEach((wrd) => {
-		countsCloudSadThought[wrd] = (countsCloudSadThought[wrd] || 0) + 1;
-	});
-	const arrCloudSadThought = [];
-	for (const [key, value] of Object.entries(countsCloudSadThought)) {
-		let objSadThought = {text: `${key}`, value: `${value}`}
-		objSadThought.value = (Number(objSadThought.value)*1000);
-		arrCloudSadThought.push(objSadThought);
+  const wordCloud = (emotion, type) => {
+	const wordsArr = [];
+
+	filterEmotion(emotion).forEach(event => {
+		const dailyWords = event[type].match(/[a-zA-Z]+/g);
+		wordsArr.push(...dailyWords)
+	})
+  const wordCloudArr = [];
+	const countsCloudEvent = {};
+	wordsArr.forEach(wrd => {
+		let val;
+		if (wrd.lenth < 3) val = 1000;
+		else if (wrd.lenth < 6) val = 3000;
+		else if (wrd.lenth < 9) val = 4000;
+		else val = 2000;
+		countsCloudEvent[wrd] = (countsCloudEvent[wrd] || 0) + val;
+	})
+	for (const [key, value] of Object.entries(countsCloudEvent)) {
+		let cloudEventObj = {};
+		cloudEventObj.text = key;
+		cloudEventObj.value = value;
+		wordCloudArr.push(cloudEventObj);
+	}
+	return wordCloudArr;
 	}
 
-// Sad Behavior
-	const arrSadBehavior = [];
-	filteringSad.forEach(oneEvent => {
-		arrSadBehavior.push(splitingSentences(oneEvent.behavior))
-		return arrSadBehavior;
-	});
-	const flattenedSadBehavior = [].concat(...arrSadBehavior);
-  	const countsCloudSadBehavior = {};
-	flattenedSadBehavior.forEach((wrd) => {
-		countsCloudSadBehavior[wrd] = (countsCloudSadBehavior[wrd] || 0) + 1;
-	});
-	const arrCloudSadBehavior = [];
-	for (const [key, value] of Object.entries(countsCloudSadBehavior)) {
-		let objSadBehavior = {text: `${key}`, value: `${value}`}
-		objSadBehavior.value = (Number(objSadBehavior.value)*1000);
-		arrCloudSadBehavior.push(objSadBehavior);
-  }
-
-//// Happy //// Happy //// Happy //// Happy //// Happy //// Happy //// Happy //// Happy
-// Happy Event
-	const arrHappyEvent = [];
-	filteringHappy.forEach(oneEvent => {
-		arrHappyEvent.push(splitingSentences(oneEvent.triggeringEvent))
-		return arrHappyEvent;
-	});
-	const flattenedHappyEvent = [].concat(...arrHappyEvent);
-	const countsCloudHappyEvent = {};
-	flattenedHappyEvent.forEach((wrd) => {
-		countsCloudHappyEvent[wrd] = (countsCloudHappyEvent[wrd] || 0) + 1;
-	});
-	const arrCloudHappyEvent = [];
-	for (const [key, value] of Object.entries(countsCloudHappyEvent)) {
-		let objHappyEvent = {text: `${key}`, value: `${value}`}
-		objHappyEvent.value = (Number(objHappyEvent.value)*1000);
-		arrCloudHappyEvent.push(objHappyEvent);
-	}
-// Happy Thought
-	const arrHappyThought = [];
-	filteringHappy.forEach(oneEvent => {
-		arrHappyThought.push(splitingSentences(oneEvent.thought))
-		return arrHappyThought;
-	});
-	const flattenedHappyThought = [].concat(...arrHappyThought);
-  const countsCloudHappyThought = {};
-  flattenedHappyThought.forEach((wrd) => {
-    countsCloudHappyThought[wrd] = (countsCloudHappyThought[wrd] || 0) + 1;
-  });
-  const arrCloudHappyThought = [];
-  for (const [key, value] of Object.entries(countsCloudHappyThought)) {
-		let obj = {text: `${key}`, value: `${value}`}
-		obj.value = (Number(obj.value)*1000);
-		arrCloudHappyThought.push(obj);
-  }
-// Happy Behavior
-	const arrHappyBehavior = [];
-	filteringHappy.forEach(oneEvent => {
-		arrHappyBehavior.push(splitingSentences(oneEvent.behavior))
-		return arrHappyBehavior;
-	});
-	const flattenedHappyBehavior = [].concat(...arrHappyBehavior);
-
-  const countsCloudHappyBehavior = {};
-  flattenedHappyBehavior.forEach((wrd) => {
-    countsCloudHappyBehavior[wrd] = (countsCloudHappyBehavior[wrd] || 0) + 1;
-  });
-  const arrCloudHappyBehavior = [];
-  for (const [key, value] of Object.entries(countsCloudHappyBehavior)) {
-		let obj = {text: `${key}`, value: `${value}`}
-		obj.value = (Number(obj.value)*1000);
-		arrCloudHappyBehavior.push(obj);
-  }
-
-	//// Anxiety 	//// Anxiety	//// Anxiety	//// Anxiety	//// Anxiety	//// Anxiety	//// Anxiety	//// Anxiety
-  //anxious event
-	const arrAnxiousEvent = [];
-	filteringAnxious.forEach(oneEvent => {
-		arrAnxiousEvent.push(splitingSentences(oneEvent.triggeringEvent))
-		return arrAnxiousEvent;
-	});
-	const flattenedAnxiousEvent = [].concat(...arrAnxiousEvent);
-  const countsCloudAnxiousEvent = {};
-	flattenedAnxiousEvent.forEach((wrd) => {
-		countsCloudAnxiousEvent[wrd] = (countsCloudAnxiousEvent[wrd] || 0) + 1;
-	});
-	const arrCloudAnxiousEvent = [];
-	for (const [key, value] of Object.entries(countsCloudAnxiousEvent)) {
-		let objAnxiousEvent = {text: `${key}`, value: `${value}`}
-		objAnxiousEvent.value = (Number(objAnxiousEvent.value)*1000);
-		arrCloudAnxiousEvent.push(objAnxiousEvent);
-  }
-
-  //anxious thought
-	const arrAnxiousThought = [];
-	filteringAnxious.forEach(oneEvent => {
-		arrAnxiousThought.push(splitingSentences(oneEvent.thought))
-		return arrAnxiousThought;
-	});
-	const flattenedAnxiousThought = [].concat(...arrAnxiousThought);
-  const countsCloudAnxiousThought = {};
-	flattenedAnxiousThought.forEach((wrd) => {
-		countsCloudAnxiousThought[wrd] = (countsCloudAnxiousThought[wrd] || 0) + 1;
-	});
-	const arrCloudAnxiousThought = [];
-	for (const [key, value] of Object.entries(countsCloudAnxiousThought)) {
-		let objAnxiousThought = {text: `${key}`, value: `${value}`}
-		objAnxiousThought.value = (Number(objAnxiousThought.value)*1000);
-		arrCloudAnxiousThought.push(objAnxiousThought);
-  }
-
-  // anxious behavior
-	const arrAnxiousBehavior = [];
-	filteringAnxious.forEach(oneEvent => {
-		arrAnxiousBehavior.push(splitingSentences(oneEvent.behavior))
-		return arrAnxiousBehavior;
-	});
-	const flattenedAnxiousBehavior = [].concat(...arrAnxiousBehavior);
-  const countsCloudAnxiousBehavior = {};
-	flattenedAnxiousBehavior.forEach((wrd) => {
-		countsCloudAnxiousBehavior[wrd] = (countsCloudAnxiousBehavior[wrd] || 0) + 1;
-	});
-	const arrCloudAnxiousBehavior = [];
-	for (const [key, value] of Object.entries(countsCloudAnxiousBehavior)) {
-		let objAnxiousBehavior = {text: `${key}`, value: `${value}`}
-		objAnxiousBehavior.value = (Number(objAnxiousBehavior.value)*1000);
-		arrCloudAnxiousBehavior.push(objAnxiousBehavior);
-  }
+ 
 
 
 ////CHOAS STOPS FROM HERE ////////////////////////////////////////////////////
@@ -222,10 +61,10 @@ export default function Journal({entry}) {
 						<Panel header="Event" key="1" className='headerjournal'>
 							<div className='collapsecontainer'>
 								<div className='textcontainer'>
-									<div>{filteringSad.map((ent) => <Entryitemevent ent={ent} key={ent.id} entry={sortDate(entry)}></Entryitemevent>)}</div>
+									<div>{filterEmotion('sad').map((ent) => <Entryitemevent ent={ent} key={ent.id} entry={sortDate(entry)}></Entryitemevent>)}</div>
 								</div>
 								<div className='cloudcontainer'>
-									<WordCloud width={500} height={500} data={arrCloudSadEvent} />
+									<WordCloud width={500} height={500} data={wordCloud('sad', 'triggeringEvent')} />
 								</div>
 							</div>
 						</Panel>
@@ -233,10 +72,10 @@ export default function Journal({entry}) {
 						<Panel header="Thought" key="2" className='headerjournal'>
 							<div className='collapsecontainer'>
 								<div className='textcontainer'>
-									<p>{filteringSad.map((ent) => <Entryitemthought ent={ent} key={ent.id}></Entryitemthought>)}</p>
+									<p>{filterEmotion('sad').map((ent) => <Entryitemthought ent={ent} key={ent.id}></Entryitemthought>)}</p>
 								</div>
 								<div className='cloudcontainer'>
-									<WordCloud width={500} height={500} data={arrCloudSadThought} />
+									<WordCloud width={500} height={500} data={wordCloud('sad', 'thought')} />
 								</div>
 							</div>
 						</Panel>
@@ -244,10 +83,10 @@ export default function Journal({entry}) {
 						<Panel header="Behaviour" key="3" className='headerjournal'>
 							<div className='collapsecontainer'>
 								<div className='textcontainer'>
-									<p>{filteringSad.map((ent) => <Entryitembehavior ent={ent} key={ent.id}></Entryitembehavior>)}</p>
+									<p>{filterEmotion('sad').map((ent) => <Entryitembehavior ent={ent} key={ent.id}></Entryitembehavior>)}</p>
 								</div>
 								<div className='cloudcontainer'>
-									<WordCloud width={500} height={500} data={arrCloudSadBehavior} />
+									<WordCloud width={500} height={500} data={wordCloud('sad', 'behavior')} />
 								</div>
 							</div>
 						</Panel>
@@ -261,10 +100,10 @@ export default function Journal({entry}) {
 					<Panel header="Event" key="1" className='headerjournal'>
 						<div className='collapsecontainer'>
 							<div className='textcontainer'>
-								<p>{filteringHappy.map((ent) => <Entryitemevent ent={ent} key={ent.id}></Entryitemevent>)}</p>
+								<p>{filterEmotion('happy').map((ent) => <Entryitemevent ent={ent} key={ent.id}></Entryitemevent>)}</p>
 							</div>
 							<div className='cloudcontainer'>
-								<WordCloud width={500} height={500} data={arrCloudHappyEvent} />
+								<WordCloud width={500} height={500} data={wordCloud('happy', 'triggeringEvent')} />
 							</div>
 						</div>
 					</Panel>
@@ -272,10 +111,10 @@ export default function Journal({entry}) {
 					<Panel header="Thought" key="2" className='headerjournal'>
 						<div className='collapsecontainer'>
 							<div className='textcontainer'>
-								<p>{filteringHappy.map((ent) => <Entryitemthought ent={ent} key={ent.id}></Entryitemthought>)}</p>
+								<p>{filterEmotion('happy').map((ent) => <Entryitemthought ent={ent} key={ent.id}></Entryitemthought>)}</p>
 							</div>
 							<div className='cloudcontainer'>
-								<WordCloud width={500} height={500} data={arrCloudHappyThought} />
+								<WordCloud width={500} height={500} data={wordCloud('happy', 'thought')} />
 							</div>
 						</div>
 					</Panel>
@@ -283,10 +122,10 @@ export default function Journal({entry}) {
 					<Panel header="Behaviour" key="3" className='headerjournal'>
 						<div className='collapsecontainer'>
 							<div className='textcontainer'>
-								<p>{filteringHappy.map((ent) => <Entryitembehavior ent={ent} key={ent.id}></Entryitembehavior>)}</p>
+								<p>{filterEmotion('happy').map((ent) => <Entryitembehavior ent={ent} key={ent.id}></Entryitembehavior>)}</p>
 							</div>
 							<div className='cloudcontainer'>
-								<WordCloud width={500} height={500} data={arrCloudHappyBehavior} />
+								<WordCloud width={500} height={500} data={wordCloud('happy', 'behavior')} />
 							</div>
 						</div>
 					</Panel>
@@ -300,10 +139,10 @@ export default function Journal({entry}) {
 						<Panel header="Event" key="1" className='headerjournal'>
 							<div className='collapsecontainer'>
 								<div className='textcontainer'>
-									<p>{filteringAnxious.map((ent) => <Entryitemevent ent={ent} key={ent.id}></Entryitemevent>)}</p>
+									<p>{filterEmotion('anxious').map((ent) => <Entryitemevent ent={ent} key={ent.id}></Entryitemevent>)}</p>
 								</div>
 								<div className='cloudcontainer'>
-									<WordCloud width={500} height={500} data={arrCloudAnxiousEvent} />
+									<WordCloud width={500} height={500} data={wordCloud('anxious', 'triggeringEvent')} />
 								</div>
 							</div>
 						</Panel>
@@ -311,10 +150,10 @@ export default function Journal({entry}) {
 						<Panel header="Thought" key="2" className='headerjournal'>
 							<div className='collapsecontainer'>
 								<div className='textcontainer'>
-									<p>{filteringAnxious.map((ent) => <Entryitemthought ent={ent} key={ent.id}></Entryitemthought>)}</p>
+									<p>{filterEmotion('anxious').map((ent) => <Entryitemthought ent={ent} key={ent.id}></Entryitemthought>)}</p>
 								</div>
 								<div className='cloudcontainer'>
-									<WordCloud width={500} height={500} data={arrCloudAnxiousThought} />
+									<WordCloud width={500} height={500} data={wordCloud('anxious', 'thought')} />
 								</div>
 							</div>
 						</Panel>
@@ -322,10 +161,10 @@ export default function Journal({entry}) {
 						<Panel header="Behaviour" key="3" className='headerjournal'>
 							<div className='collapsecontainer'>
 								<div className='textcontainer'>
-									<p>{filteringAnxious.map((ent) => <Entryitembehavior ent={ent} key={ent.id}></Entryitembehavior>)}</p>
+									<p>{filterEmotion('anxious').map((ent) => <Entryitembehavior ent={ent} key={ent.id}></Entryitembehavior>)}</p>
 								</div>
 								<div className='cloudcontainer'>
-									<WordCloud width={500} height={500} data={arrCloudAnxiousBehavior} />
+									<WordCloud width={500} height={500} data={wordCloud('anxious', 'behavior')} />
 								</div>
 							</div>
 						</Panel>
